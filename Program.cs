a006d57b341806c04de05459b0998602adf936cd
Ilya Shevchyk <il2001sh@gmail.com>
2021-05-24 16:57:48 +0300

    using proxy

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
+
 using Fasade;
+using Proxy_;
 
 namespace mapz_4
 {
@@ -9,10 +12,28 @@ namespace mapz_4
         {
             DataManager dataManager = new DataManager();
             ReviewManager reviewManager = new ReviewManager();
-
             RatingFacade ratingPortal = new RatingFacade(dataManager, reviewManager);
             ratingPortal.Start();
 
+            List<string> resourses = new() { "EBEC", "Chess tournament", "WIPZ KR" };
+
+            UserRequest request = new UserRequest();
+
+            System.Console.WriteLine("\nSimulating work for unregistered User");
+            foreach (var res in resourses)
+            {
+                RealResource realResource = new RealResource(res);
+                Proxy proxy = new Proxy(realResource, 0); //unregistered user, wirh accessLevel = 0
+                request.Get(proxy);
+            }
+
+            System.Console.WriteLine("\nSimulating work for registered User");
+            foreach (var res in resourses)
+            {
+                RealResource realResource = new RealResource(res);
+                Proxy proxy = new Proxy(realResource, 1); //unregistered user, wirh accessLevel = 0
+                request.Get(proxy);
+            }
 
         }
     }
